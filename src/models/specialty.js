2f3a72c63373bf7671ab01dc16d519b9676cdeaa
Lâm Bùi Quốc <22520733@ms.uit.edu.vn>
2024-10-12 02:30:20 +0700

    1

--- src/models/specialty.js
+++ src/models/specialty.js
@@ -1,7 +1,5 @@
- 'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Specialty extends Model {
     /**
@@ -12,15 +10,17 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Specialty.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    image: DataTypes.STRING,
-
-  }, {
-    sequelize,
-    modelName: 'Specialty',
-  });
+  }
+  Specialty.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Specialty",
+    }
+  );
   return Specialty;
 };

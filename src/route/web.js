a2e1c48dd294a0bb5054c6cde589130307903703
noahihi203 <da02470247@example.com>
2024-10-15 23:42:57 +0700

    ✅ video 52

--- src/route/web.js
+++ src/route/web.js
@@ -4,22 +4,23 @@ import userController from "../controller/userController";
 let router = express.Router();
 
 let initWebRouters = (app) => {
-    router.get('/', homeController.getHomePage);
-    router.get('/crud', homeController.getCRUD);
+  router.get("/", homeController.getHomePage);
+  router.get("/crud", homeController.getCRUD);
 
-    router.post('/post-crud', homeController.postCRUD);
-    router.get('/get-crud', homeController.displayGetCRUD);
-    router.get('/edit-crud', homeController.getEditCRUD);
-    router.post('/put-crud', homeController.putCRUD);
-    router.get('/delete-crud', homeController.getDeleteCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/delete-crud", homeController.getDeleteCRUD);
 
-    router.post('/api/login', userController.handleLogin);
-    router.get('/api/get-all-users', userController.handleGetAllUsers);
-    router.post('/api/create-new-user', userController.handleCreateNewUser);
-    router.put('/api/edit-user', userController.handleEditUser);
-    router.delete('/api/delete-user', userController.handleDeleteUser);
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
 
-    return app.use("/", router);
-}
+  router.get("/allcode", userController.getAllCode);
+  return app.use("/", router);
+};
 
-module.exports = initWebRouters;    
+module.exports = initWebRouters;

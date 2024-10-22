7a65277bd8ca95c308d70dac35c2c85e5b1386ef
noahihi203 <da02470247@gmail.com>
2024-10-22 20:19:30 +0700

    video 69

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -99,7 +99,7 @@ let getDetailDoctorById = (inputId) => {
       } else {
         let data = await db.User.findOne({
           where: { id: inputId },
-          attributes: { exclude: ["password", "image"] },
+          attributes: { exclude: ["password"] },
           include: [
             {
               model: db.Markdown,
@@ -111,9 +111,14 @@ let getDetailDoctorById = (inputId) => {
               attributes: ["valueEn", "valueVi"],
             },
           ],
-          raw: true,
+          raw: false,
           nest: true,
         });
+
+        if (data && data.image) {
+          data.image = new Buffer(data.image, "base64").toString("binary");
+        }
+        if (!data) data = {};
         resolve({
           errCode: 0,
           data: data,
